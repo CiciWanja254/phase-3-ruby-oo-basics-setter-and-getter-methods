406e0ea96dce02f391ad2f983e99131d51a0d372
Cici254 <thukucindy00@gmail.com>
2023-05-18 21:15:47 +0300

    Complete asssignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-
-end
+    # getter and setter for name
+    def name
+      @name
+    end
+  
+    def name=(value)
+      @name = value
+    end
+  
+    # getter and setter for breed
+    def breed
+      @breed
+    end
+  
+    def breed=(value)
+      @breed = value
+    end
+  end
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
-end
+    # getter and setter for name
+    def name
+      @name
+    end
+  
+    def name=(value)
+      @name = value
+    end
+  
+    # getter and setter for job
+    def job
+      @job
+    end
+  
+    def job=(value)
+      @job = value
+    end
+  end
+  
